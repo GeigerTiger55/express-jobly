8cb6af399f892f99761243fd4924cb2b4f277c0b
GeigerTiger55 <elise.geiger@gmail.com>
2022-07-06 12:32:55 -0700

    Added docstring for sqlForPartialUpdate function

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,7 +1,27 @@
+"use strict";
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/** Creates sql script to map fields that are being updated to their new values.
+ * 
+ * Accepts:
+ * - "dataToUpdate" includes (JavaScript) key to value object of fields and the 
+ * new replacement values:
+ *  EX: { lastName: "NewLastName", email: "newemail@g.com" }
+ * 
+ * - "jsToSql" is an object containing keys in JavaScript syntax mapped to values
+ * for the corresponding fields in the table. Only contains keys/fields with names
+ * differing between the JavaScript and table.
+ *  EX: { lastName: last_name }
+ * 
+ * Returns object containing:
+ * - "setCols": sql script of columns mapped to values array indexes (does not 
+ * include columns that are not being updated):
+ *  EX: "last_name=$1, email=$2"
+ * 
+ * - "values": array of the new values in order of the columns being updated
+ *  EX: [ "NewLastName", "newemail@g.com" ]
+ * 
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
